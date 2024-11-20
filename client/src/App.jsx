454addd0f33fcfc34b976d6f10d08181a833265d
Joe <chanaphat.sip@gmail.com>
2024-11-21 05:05:01 +0700

    update(app): add header, searchbox and posts component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import Posts from "./components/Posts"
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  
+  return (
+    <div className="App">
+      <header>เที่ยวไหนดี</header>
+      <div className="search-box">
+        <label htmlFor="search">ค้นหาที่เที่ยว</label>
+        <input id="search" type="text" placeholder="หาที่เที่ยวแล้วไปกัน ..."/>
+      </div>
+      <div className="card">
+      <Posts/>
+      </div>
+    </div>
+  )
 }
 
 export default App;
