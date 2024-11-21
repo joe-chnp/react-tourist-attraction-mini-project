75b100335eb106223232bfd9e74575ab1b4e36e3
Joe <chanaphat.sip@gmail.com>
2024-11-21 07:12:02 +0700

    update: posts component

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -8,20 +8,43 @@ function Posts() {
   const main = async () => {
     try {
       const response = await axios.get("http://localhost:4001/trips?keywords=");
-      setTrips(response.data.data);
+      setTrips(response?.data?.data);
     } catch (error) {
       console.error("Error fetching trips data:", error);
     }
   };
-
+  
   useEffect(() => {
     main ();
   }, []);
-  console.log(trips)
+
   return (
-    <div>
-     
+    <div className="posts-container">
+      {trips.map((trip, index) => {
+        return (
+          <div key={index} className="post">
+           <img src={trip?.photos[0]}/>
+           <div className="details">
+            <h1>{trip?.title}</h1>
+            <p>{trip?.description}</p>
+            <a>อ่านต่อ</a>
+            <div className="tags">
+              <span>หมวด</span>
+              {trip?.tags.map((tag, index) => {
+                return <a key={index}>{tag}</a>
+              })}
+            </div>
+            <div className="imgs-container">
+              {trip?.photos.slice(1).map((photo, index) => {
+                return <img key={index} src={photo}/>
+              })}
+            </div>
+           </div>
+          </div>
+        )
+      })}
     </div>
+    
   )
 }
 
