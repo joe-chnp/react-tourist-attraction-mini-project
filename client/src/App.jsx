cc33baefe624a23c41a9f40e61c6bd1e415a848d
Joe <chanaphat.sip@gmail.com>
2024-11-22 22:37:19 +0700

    update(app): add search feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
 import "./App.css";
 import Posts from "./components/Posts"
+import { useState } from "react";
 
 function App() {
+  const [search, setSearch] = useState("");
   
   return (
     <div className="App">
       <header>เที่ยวไหนดี</header>
       <div className="search-box">
         <label htmlFor="search">ค้นหาที่เที่ยว</label>
-        <input id="search" type="text" placeholder="หาที่เที่ยวแล้วไปกัน ..."/>
+        <input id="search" type="text" placeholder="หาที่เที่ยวแล้วไปกัน ..." value={search} onChange={(event) => {setSearch(event.target.value)}}/>
       </div>
       <div className="card">
-      <Posts/>
+      <Posts search={search} setSearch={setSearch}/>
       </div>
     </div>
   )
